3f45d1c3374e7283d71ddf4f5666e3bb8c9270cd
Grellinger <=>
2016-10-02 11:05:16 -0500

    initial commit of first test

---
+++ src/main/test/grellinger/bopm/BinomialOptionsTreeTest.java
@@ -0,0 +1,14 @@
+package grellinger.bopm;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class BinomialOptionsTreeTest {
+
+	@Test
+	public void thisTestShouldAlwaysPass() {
+		assertTrue(true);
+	}
+	
+}
